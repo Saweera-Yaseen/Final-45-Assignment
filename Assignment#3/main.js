d7adc7de6ef201998379b598f0ed8ce72270c5de
Saweera Yaseen <sufyanfatima62@gmail.com>
2024-03-06 19:43:57 +0500

    first commit

---
+++ Assignment#3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing a famous quote and its author
+let quote = "Nothing is impossible because impossible itself T M possible";
+let author = "Saweera";
+//printing the message 
+console.log(`"${quote}" - ${author}`);
---
+++ Assignment#3/main.ts
@@ -0,0 +1,6 @@
+//storing a famous quote and its author
+
+let quote:string="Nothing is impossible because impossible itself I M possible";
+let author:string="Saweera";
+//printing the message 
+console.log(`"${quote}" - ${author}`);
